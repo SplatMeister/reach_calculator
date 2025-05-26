f6070bc50fbf3b859cfff1a82ddb0940670af867
Rohan Perera <97901167+SplatMeister@users.noreply.github.com>
2025-05-26 17:57:36 +0530

    app.py

--- app.py
+++ app.py
@@ -6,15 +6,16 @@ from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 
 st.set_page_config(page_title="Budget Optimum Detection", layout="centered")
-# Display the Ogilvy logo from the direct URL
+# Display the Ogilvy logo from the direct URL (new link)
 st.markdown(
     """
     <div style="text-align: center;">
-        <img src="https://pohlmann.design/wp-content/uploads/2022/02/OgilvyTartan-scaled.jpg" width="300">
+        <img src="https://www.ogilvy.com/sites/g/files/dhpsjz106/files/inline-images/Ogilvy%20Restructures.jpg" width="300">
     </div>
     """,
     unsafe_allow_html=True
 )
+
 #st.title("Optimum Budget Detection – Meta, Google & TV Data")
 # Main Title
 st.title("Optimum Budget Detection")
